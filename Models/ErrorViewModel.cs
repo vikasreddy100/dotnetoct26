f2f775e8c9d8db43326f027abd81b520d93883f2
Vikas <vikasreddy100@gmal.com>
2021-10-26 13:15:16 +0530

    My First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorld.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
